07241fa68a9b81d30b9b8887ff0bdd82000895e1
Wesley Felipe Gouveia Soares <WesleyFelipe@academicos.com>
2025-04-07 21:07:49 -0300

    início: home.tsx

---
+++ vite-project/src/assets/home.tsx
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <img id="header" src="images/logo.png" alt="">
+    </div>
+    <div id="botao">
+        <a href="https://api.chucknorris.io/jokes/random">Link</a>
+    </div>
+</body>
+</html>
